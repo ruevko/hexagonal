48393d4f6612cebb90137fb9c9a268a5461800b9
unknown <ruevko@gmail.com>
2020-01-14 22:08:28 -0500

    logo al fin

--- R/my_new_logo.R
+++ R/my_new_logo.R
@@ -1,56 +1,74 @@
-library(dplyr); library(ggplot2); theme_set(theme_void())
+library(dplyr); library(ggplot2)
 
-hex = tibble(r = rep(c(1, 5/6), 6), t = rep(pi*0:5/3 - pi/6, each = 2)) %>%
+########
+def = ggplot(mapping = aes(x, y, label = i)) + coord_fixed(expand = FALSE) + theme_void()
+
+hex = tibble(r = rep(c(1, 6/7), 6), t = rep(pi*(0:5/3 - 1/6), each = 2)) %>%
    transmute(x = r*cos(t), y = r*sin(t), i = row_number())
 
-# hex[c(rep(c(-1, 0, -1), 6) + rep(2*1:6, each = 3), 1, 2*1:6, 2), ] %>%
-#    ggplot(aes(x, y)) + coord_fixed(1) + geom_path()
+#def + geom_path(data = hex[c(rep(c(-1, 0, -1), 6) + rep(2*1:6, each=3), 1, 2*1:6, 2), ])
 
+hex = bind_rows(tibble(x = c(rep(.7423, 3), rep(.6186, 4)),
+                       y = c(-.5714, 0, .5714, .5, .1237, -.1237, -.5)), hex)
 
-hex = head(hex, 2) %>% mutate(y = hex$y[1] - y) %>% bind_rows(hex)
+hex = mutate(hex[1:11, ], x = hex$x[1]-x) %>% bind_rows(hex) %>% mutate(i = row_number())
 
-hex = hex[c(6, 1:14), ]; hex$y[1] = hex$y[6] - hex$y[7]
+hex$y[c(1, 3, 4, 7:11)] = .95*hex$y[c(1, 3, 4, 7:11)]
 
-hex = head(hex, 7) %>% mutate(x = hex$x[1] - x) %>% bind_rows(hex) %>% mutate(i = 1:22)
+#def + geom_text(data = hex)
 
-hex$y[4:7] = 5/6*hex$y[4:7]; hex$x[1:7] = hex$x[1:7] - mean(hex$x[1:2])
+g = c(13,17,18,12,18,30,29,30,28,27,28,26,25,26,24,23,24,22,14,23,25,27,29,12,13,2,6,17)
+h = c(22,15,16,5,4,3,10,8,1,7,6,17,18,12,19,21,14,12,13,17,13,16,13,2,6,2,5,2,3,1)
 
-# ggplot(hex, aes(x, y, label = i)) + coord_fixed(1) + geom_text()
+#def + geom_path(data = hex[h, ]) + geom_path(data = hex[g, ]) + xlim(c(-.9, .9))
 
+hexo = mutate(hex[17 + 2*1:6, ], x = 1.05*x, y = 1.05*y)
 
-way = c(1, 7, 6, 4, 5, 3, 10, 12, 22, 20, 18, 16, 14, 8, 9, 13, 14, 13, 15, 16,
-        15, 17, 18, 17, 19, 20, 19, 21, 22, 21, 11, 12, 11, 9, 10, 9, 8, 1, 2, 3)
+let = c("a","í","r","e","l","a",".","La",".","e","x","a","g","o","n","a","l") %>%
+   tibble(i = ., r = 3/5, t = pi*1:17/9) %>% mutate(x = r*cos(t), y = r*sin(t))
 
-# hex[c(way[1:16], 15, 17, 19, 21, 11, 9, 8), ] %>%
-#    ggplot(aes(x, y)) + coord_fixed(1) + geom_polygon()
-#
-# ggsave("static/favicon.png", bg = NA, width = 8/15, height = 8/15)
+def + #geom_polygon(data = hexo, fill = "grey95") +
+   #geom_text(data = let, family = "mono", col = "grey") +
+   geom_polygon(data = hex[h[1:17], ], fill = "grey") + #geom_path(data = hex[h, ]) +
+   geom_polygon(data = hex[g, ], fill = "grey40") #+ geom_path(data = hex[g, ])
 
+#ggsave("static/sticker.svg", width = sqrt(3), height = 2)
+ggsave("static/favicon.png", bg = NA, height = 1, width = 1)
+# claro ####
 
-let = c("a", "í", "r", "e", "l", "a", ".", "La", ".",
-        "e", "x", "a", "g", "o", "n", "a", "l") %>%
-   tibble(i = ., r = 3/5, t = pi*1:17/9) %>%
-   mutate(x = r*cos(t), y = r*sin(t))
+def + geom_polygon(data = hexo, fill = NA) +
+   geom_polygon(data = hex[17 + 2*1:6, ], fill = "grey95") +
+   geom_polygon(data = hex[c(13, 16, 15, 22), ], fill = "grey60") +
+   geom_polygon(data = hex[c(1, 3, 10, 8), ], fill = "grey60") +
+   geom_polygon(data = hex[c(12, 14, 21, 19), ], fill = "grey") +
+   geom_polygon(data = hex[c(1, 2, 6, 7), ], fill = "grey") +
+   geom_polygon(data = hex[c(2:5, 16, 13), ], fill = "grey80") +
+   geom_polygon(data = hex[c(14, 22, 24, 23), ]) +
+   geom_polygon(data = hex[c(23, 24, 26, 25), ], fill = "grey30") +
+   geom_polygon(data = hex[c(25, 26, 28, 30, 29, 27), ], fill = "grey40") +
+   geom_polygon(data = hex[c(29, 30, 18, 12), ], fill = "grey30") +
+   geom_polygon(data = hex[c(17, 18, 12, 13, 2, 6), ]) +
+   geom_text(data = let, col = "grey85", family = "mono") +
+   geom_path(data = hex[h, ]) + geom_path(data = hex[g, ])
 
-# ggplot(hex[way, ], aes(x, y)) + coord_fixed(1) + geom_path() +
-#    geom_text(aes(label = i), let, family = "mono")
-#
-# ggsave("static/figure.png", bg = NA, width = 2, height = 2, dpi = "retina")
+ggsave("static/logo.png", bg = NA, height = 2, width = 2) #sqrt(3)
 
+# scuro ####
 
-ggplot(mapping = aes(x, y)) + coord_fixed(1) +
-   geom_polygon(data = hex[2*6:11, ], fill = "grey85") +
-   geom_polygon(data = hex[c(8:9, 13:14), ], fill = "grey60") +
-   geom_polygon(data = hex[c(4, 5, 7, 6), ], fill = "grey60") +
-   geom_polygon(data = hex[c(1:3, 10:8), ], fill = "grey30") +
-   geom_polygon(data = hex[c(1, 2, 9, 8), ], fill = "grey45") +
-   geom_polygon(data = hex[c(9, 10, 12, 11), ], fill = "grey35") +
-   geom_polygon(data = hex[c(21, 22, 12, 11), ], fill = "grey40") +
-   geom_polygon(data = hex[c(19, 20, 22, 21), ], fill = "grey45") +
-   geom_polygon(data = hex[c(17, 18, 20, 19), ], fill = "grey40") +
-   geom_polygon(data = hex[c(15, 16, 18 ,17), ], fill = "grey35") +
-   geom_polygon(data = hex[c(13, 14, 16, 15), ], fill = "grey30") +
-   geom_text(aes(label = i), let, col = "white", family = "mono") #, size = 1
+def + geom_polygon(data = hexo, fill = NA) +
+   geom_polygon(data = hex[17 + 2*1:6, ], fill = "grey5") +
+   geom_polygon(data = hex[c(2:5, 16, 13), ], fill = "grey25") +
+   geom_polygon(data = hex[c(12, 14, 21, 19), ], fill = "grey25") +
+   geom_polygon(data = hex[c(13, 16, 15, 22), ], fill = "grey15") +
+   geom_polygon(data = hex[c(1, 3, 10, 8), ], fill = "grey15") +
+   geom_polygon(data = hex[c(1, 2, 6, 7), ]) +
+   geom_polygon(data = hex[c(14, 22, 24, 23), ], fill = "grey60") +
+   geom_polygon(data = hex[c(23, 24, 26, 25), ], fill = "grey") +
+   geom_polygon(data = hex[c(25, 26, 28, 30, 29, 27), ], fill = "grey85") +
+   geom_polygon(data = hex[c(29, 30, 18, 12), ], fill = "grey") +
+   geom_polygon(data = hex[c(17, 18, 12, 13, 2, 6), ], fill = "grey60") +
+   geom_text(data = let, col = "grey20", family = "mono", size = 1) +
+   geom_path(data = hex[h, ], col = "grey5") +
+   geom_path(data = hex[g, ], col = "grey5")
 
-ggsave("static/sticker.png", bg = NA, width = sqrt(3), height = 2)
-# ggsave("static/logo.png", type = "cairo-png", bg = NA, width = .35*sqrt(3), height = .7)
+#ggsave("static/logo2.png", type = "cairo-png", bg = NA, height = .7, width = .35*sqrt(3))
---
+++ content/page.md
@@ -0,0 +1,32 @@
+---
+
+author: Rafael Castro
+date: '2020-01-01'
+slug: page
+
+comments: no
+showMeta: no
+showActions: no
+---
+
+> "El universo (que otros llaman la Biblioteca) se compone de un número indefinido, y tal vez infinito, de galerías hexagonales, con vastos pozos de ventilación en el medio, cercados por barandas bajísimas. Desde cualquier hexágono se ven los pisos
+>
+> inferiores y superiores: interminablemente."
+>
+> **Jorge Luis Borges (1941)**
+
+### Acerca de
+
+Hay dos razones detrás del nombre de este blog. La primera es un tributo a la maestría de Borges al vislumbrar -en [uno](https://es.wikipedia.org/wiki/La_biblioteca_de_Babel) de sus cuentos- al conocimiento como algo finito, pero exorbitantemente grande a la vez. Una biblioteca que posea todo el conocimiento es posible (con la colaboración de muchas personas), aunque haga falta un gran número de fuentes para contenerla. Este blog puede ser una de esas fuentes.
+
+La segunda razón se desprende de [cierta](http://hexb.in/) costumbre entre programadores, de diseñar logos hexagonales para software, proyectos, grupos, etc. En lenguaje R esta costumbre ha sido muy adoptada; y dado que aquí los análisis, las publicaciones y el blog mismo se construyen en R, la adoptamos igualmente. Además, esos logos resuenan bastante bien con las galerías de la biblioteca total.
+
+### Créditos
+
+- Louis Barranqueiro. [Tranquilpeak](https://louisbarranqueiro.github.io/hexo-theme-tranquilpeak/): tema web Hexo; Thibaud Leprêtre lo adaptó a Hugo.
+- Bjørn Erik Pedersen, Steve Francia, et al. [Hugo](https://gohugo.io/): generador de sitios web estáticos.
+- Yihui Xie, Amber Thomas y Alison Presmanes Hill. [blogdown](https://bookdown.org/yihui/blogdown/): paquete de lenguaje R que permite construir el sitio web estático.
+- Yihui Xie también diseñó otros paquetes necesarios: [bookdown](https://bookdown.org/yihui/bookdown/), [servr](https://cran.rstudio.com/web/packages/servr/index.html), entre otros.
+- La gente de RStudio Inc. [RStudio](https://rstudio.com/): entorno de desarrollo integrado.
+- La gente del R Core Team. [R](https://www.R-project.org/): el lenguaje, por supuesto.
+- Y la gente de GitHub, Inc. [GitHub Pages](https://pages.github.com/): donde reside este blog.
