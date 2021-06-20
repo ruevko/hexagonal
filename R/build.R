ffa8a3743ab5ad1de93d3d592ca8fd13f72d3f63
Rafael Castro <ruevko@gmail.com>
2021-06-19 21:10:22 -0500

    Updated: new REFS

--- R/build.R
+++ R/build.R
@@ -12,30 +12,46 @@ for (rmdFile in dir('content/post', '\\.Rmd$', full.names = TRUE, recursive = TR
 
 for (htmlFile in dir('content/post', '\\.html$', full.names = TRUE, recursive = TRUE)) {
 
-   htmlLines = readLines(htmlFile)
+   htmlWrite = FALSE; htmlLines = readLines(htmlFile)
+
+   REFLine = which(htmlLines == '<div id="refs" class="references">')
+
+   if (length(REFLine) == 1) {
+
+      htmlLines = c(htmlLines[1 : {REFLine - 1}],
+                    '<span class="post-meta">Referencias</span><div id="refs" class="references">',
+                    htmlLines[ - {1 : REFLine}])
+
+      htmlWrite = TRUE; message(crayon::green(sub('content/post/', 'Fixed REF in ', htmlFile)))
+
+   }
 
    TOCLine = which(htmlLines == '<div id="TOC">')
 
    if (length(TOCLine) == 1) {
 
       htmlLines = c(htmlLines[1 : {TOCLine - 1}],
-                    '<div id="TOC"><h1 style="margin: 8px 0px;">Contenido</h1>',
+                    '<div id="TOC"><span class="post-meta">Contenido</span>',
                     htmlLines[ - {1 : TOCLine}])
 
-      message(crayon::blue(sub('content/post/....', 'Fixed TOC in ', htmlFile)))
+      htmlWrite = TRUE; message(crayon::blue(sub('content/post/', 'Fixed TOC in ', htmlFile)))
 
    }
 
    VOFLine = which(htmlLines == '.sourceCode { overflow: visible; }')
 
-   if (length(VOFLine) > 0) {
+   if (length(VOFLine) == 1) {
 
       htmlLines = htmlLines[ - VOFLine]
 
-      message(crayon::cyan(sub('content/post/....', 'Fixed VOF in ', htmlFile)))
+      htmlWrite = TRUE; message(crayon::cyan(sub('content/post/', 'Fixed VOF in ', htmlFile)))
 
    }
 
-   writeLines(htmlLines, htmlFile)
+   if(htmlWrite) writeLines(htmlLines, htmlFile) else {
+
+      message(crayon::yellow(sub('content/post/', 'All is OK in ', htmlFile)))
+
+   }
 
 }
